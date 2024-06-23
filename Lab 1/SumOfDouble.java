9bdea97762732824c9b5be94a1f84acdd91be591
Jasmin Chauhan <146665572+JasminChauhan@users.noreply.github.com>
2024-06-23 10:52:46 +0530

    Add files via upload

---
+++ Lab 1/SumOfDouble.java
@@ -0,0 +1,8 @@
+public class SumOfDouble {
+    public static void main(String[] args){
+        double a = Double.parseDouble(args[0]);
+        double b = Double.parseDouble(args[1]);
+        double c = a + b;
+        System.out.println("The sum of two numbers is: "+c);
+    }
+}
